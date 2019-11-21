5ab26ee30e43d77d1a8edc4623518c28d737489d
Christian Pippin <ianpippin@gmail.com>
2019-11-21 15:18:42 -0600

    created add functionality in animals

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -9,7 +9,7 @@ import AnimalDetail from './animal/AnimalDetail'
 import EmployeeDetail from './employees/EmployeeDetail'
 import LocationDetail from './location/LocationDetail'
 import OwnerDetail from './owner/OwnerDetail'
-//only include these once they are built - previous practice exercise
+import AnimalForm from './animal/AnimalForm'
 
 
 class ApplicationViews extends Component {
@@ -20,15 +20,17 @@ class ApplicationViews extends Component {
         <Route exact path="/" render={(props) => {
           return <Home />
         }} />
-        {/* Make sure you add the `exact` attribute here */}
         <Route exact path="/animals" render={(props) => {
-          return <AnimalList />
+          return <AnimalList {...props} />
+        }} />
+        <Route path="/animals/new" render={(props) => {
+          return <AnimalForm {...props} />
         }} />
         <Route path="/animals/:animalId(\d+)" render={(props) => {
           // Pass the animalId to the AnimalDetailComponent
-          return <AnimalDetail 
-          animalId={parseInt(props.match.params.animalId)} 
-          {...props}
+          return <AnimalDetail
+            animalId={parseInt(props.match.params.animalId)}
+            {...props}
           />
         }} />
 
@@ -45,9 +47,9 @@ class ApplicationViews extends Component {
         }} />
         <Route path="/employees/:employeeId(\d+)" render={(props) => {
           // Pass the employeeId to the EmployeeDetailComponent
-          return <EmployeeDetail 
-          employeeId={parseInt(props.match.params.employeeId)} 
-          {...props}
+          return <EmployeeDetail
+            employeeId={parseInt(props.match.params.employeeId)}
+            {...props}
           />
         }} />
         <Route exact path="/owners" render={(props) => {
@@ -55,9 +57,9 @@ class ApplicationViews extends Component {
         }} />
         <Route path="/owners/:ownerId(\d+)" render={(props) => {
           // Pass the ownerId to the OwnerDetailComponent
-          return <OwnerDetail 
-          ownerId={parseInt(props.match.params.ownerId)} 
-          {...props}
+          return <OwnerDetail
+            ownerId={parseInt(props.match.params.ownerId)}
+            {...props}
           />
         }} />
         <Route exact path="/locations" render={(props) => {
@@ -65,9 +67,9 @@ class ApplicationViews extends Component {
         }} />
         <Route path="/locations/:locationId(\d+)" render={(props) => {
           // Pass the locationId to the LocationDetailComponent
-          return <LocationDetail 
-          locationId={parseInt(props.match.params.locationId)} 
-          {...props}
+          return <LocationDetail
+            locationId={parseInt(props.match.params.locationId)}
+            {...props}
           />
         }} />
       </>

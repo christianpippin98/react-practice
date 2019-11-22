d871d451085940e7197526b4b41755500ec8a3b7
Christian Pippin <ianpippin@gmail.com>
2019-11-22 15:36:14 -0600

    created add and edit sections for employees

--- src/components/employees/EmployeeCard.js
+++ src/components/employees/EmployeeCard.js
@@ -6,14 +6,16 @@ class EmployeeCard extends Component {
   render() {
     return (
       <div className="card">
-          <div className="card-content">
-            <picture>
-              <img src={require('./employee.png')} alt="Employee" />
-            </picture>
-            <h2>Name: <span className="card-employeename">{this.props.employee.name}</span></h2>
-            <button type="button" onClick={() => this.props.deleteEmployee(this.props.employee.id)}>Fire</button>
-            <Link to={`/employees/${this.props.employee.id}`}><button>Details</button></Link>
-          </div>
+        <div className="card-content">
+          <picture>
+            <img src={require('./employee.png')} alt="Employee" />
+          </picture>
+          <h2>Name: <span className="card-employeename">{this.props.employee.name}</span></h2>
+          <button type="button"
+            onClick={() => { this.props.history.push(`/employees/${this.props.employee.id}/edit`) }}>Edit</button>
+          <button type="button" onClick={() => this.props.deleteEmployee(this.props.employee.id)}>Fire</button>
+          <Link to={`/employees/${this.props.employee.id}`}><button>Details</button></Link>
+        </div>
       </div>
     );
   }
--- src/modules/AnimalManager.js
+++ src/modules/AnimalManager.js
@@ -2,28 +2,37 @@ const remoteURL = "http://localhost:5002"
 
 export default {
   get(id) {
-    return fetch(`${remoteURL}/animals/${id}`).then(result => 
-    result.json())
+    return fetch(`${remoteURL}/animals/${id}`).then(result =>
+      result.json())
   },
   getAll() {
-    return fetch(`${remoteURL}/animals`).then(result => 
-    result.json())
+    return fetch(`${remoteURL}/animals`).then(result =>
+      result.json())
   },
   delete(id) {
     return fetch(`http://localhost:5002/animals/${id}`, {
-        method: "DELETE"
+      method: "DELETE"
     })
-    .then(result => result.json())
+      .then(result => result.json())
   },
   post(newAnimal) {
     return fetch(`${remoteURL}/animals`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(newAnimal)
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newAnimal)
     }).then(data => data.json())
-}
+  },
+  update(editedAnimal) {
+    return fetch(`${remoteURL}/animals/${editedAnimal.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(editedAnimal)
+    }).then(data => data.json());
+  }
 }
 
 

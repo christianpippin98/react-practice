682c9d9f484ff523c3c4f2b754b5734cb4443541
Christian Pippin <ianpippin@gmail.com>
2019-11-25 12:48:01 -0600

    Created dropdown and functionality

--- src/components/employees/EmployeeCard.js
+++ src/components/employees/EmployeeCard.js
@@ -14,7 +14,8 @@ class EmployeeCard extends Component {
           <button type="button"
             onClick={() => { this.props.history.push(`/employees/${this.props.employee.id}/edit`) }}>Edit</button>
           <button type="button" onClick={() => this.props.deleteEmployee(this.props.employee.id)}>Fire</button>
-          <Link to={`/employees/${this.props.employee.id}`}><button>Details</button></Link>
+          <button type="button"
+            onClick={() => { this.props.history.push(`/employees/${this.props.employee.id}/details`) }}>Details</button>
         </div>
       </div>
     );
